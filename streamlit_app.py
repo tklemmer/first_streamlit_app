a20f0fbd4914c0007b9a906423c1f8f28021b197
tklemmer <timklemmer@gmail.com>
2023-06-14 11:20:23 -0400

    Update streamlit_app.py
    another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
