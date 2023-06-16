c14eefd03ff0957498d1ffc892dccf802e76937d
tklemmer <timklemmer@gmail.com>
2023-06-15 21:10:39 -0400

    Update streamlit_app.py
    no connector code

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-snowflake-connector-python
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
