6c6172e0b2d397d0f1a8c020d9bfe0c77708f62d
tklemmer <timklemmer@gmail.com>
2023-06-15 21:02:12 -0400

    Update streamlit_app.py
    revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,10 +45,3 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("Fruit load list contains:")
-streamlit.text(my_data_row)
