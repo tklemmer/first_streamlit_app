7efbf827153df93e3c7496113267c2fdfd01bb2a
tklemmer <timklemmer@gmail.com>
2023-06-16 17:47:05 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list calues ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
