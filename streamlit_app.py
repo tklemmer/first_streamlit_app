4e9c718b51232097524adcf52530ce3ba6e06d1b
tklemmer <timklemmer@gmail.com>
2023-06-15 21:15:03 -0400

    Update streamlit_app.py
    all not none

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
