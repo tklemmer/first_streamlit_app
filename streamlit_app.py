b6aa3bf14a60ed77d68b4b5922fa03e33328111a
tklemmer <timklemmer@gmail.com>
2023-06-16 17:40:44 -0400

    Update streamlit_app.py
    get data from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,10 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.text("Hello from Snowflake:")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
+streamlit.write('Thanks for adding ', add_my_fruit)
