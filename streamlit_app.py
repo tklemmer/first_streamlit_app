4ba1d042f8bd4b0f680ae12392a284b463921f8a
tklemmer <timklemmer@gmail.com>
2023-06-15 21:14:01 -0400

    Update streamlit_app.py
    added new select

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
