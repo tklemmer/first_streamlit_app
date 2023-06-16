a572baae628a0370ae52055c36b05fd6e05fdf4d
tklemmer <timklemmer@gmail.com>
2023-06-16 18:11:45 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
       fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
 
-streamlit.header('Fruityvice Fruit Advice!');
+streamlit.header('Fruityvice Fruit Advice!')
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
