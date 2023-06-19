369db7d4d649a84106eaa6d3e988c216cc1da37c
tklemmer <timklemmer@gmail.com>
2023-06-19 08:48:05 -0400

    Update streamlit_app.py
    removed stop adding fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if streamlit.button('Get Fruit Load List'):
    streamlit.dataframe(my_data_rows)
 
    
-streamlit.stop()
+#streamlit.stop()
 
 # Allow the end user to add a fruit
 def insert_row_snowflake(new_fruit):
